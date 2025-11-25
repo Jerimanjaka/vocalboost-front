3bda621138fadb0b57f3bba1c47a259ed0f52a60
Jerimanjaka Tsiori <randrianasolo.jerimanjaka@gmail.com>
2025-11-25 01:53:19 +0100

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.audiorecorder.app',
   appName: 'audio-recorder-app',
-  webDir: 'dist/audio-recorder-app/browser'
+  webDir: 'dist/audio-recorder-app/browser',
+  bundledWebRuntime: false
 };
 
 export default config;
